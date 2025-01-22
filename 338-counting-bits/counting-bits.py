1adca06d8b8dc2ea025aa3b86ef9ce423bfbc19b
Eddie Chege <99240689+Eddychege@users.noreply.github.com>
2025-01-22 09:25:42 +0300

    Time: 7 ms (52.11%) | Memory: 18.5 MB (59.64%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,10 +1,11 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         res=[0]*(n+1)
+        offset=1
         for i in range(1,n+1):
-            if i%2==0:
-                res[i]=res[i>>1]
-            else:
-                res[i]=res[i-1]+1
+            if offset*2==i:
+                offset=i
+            res[i]=1+res[i-offset]
+    
         return res
         
