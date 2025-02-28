c1cce2eeb29322784c88ea19d476defbb1caa39c
Eddie Chege <99240689+Eddychege@users.noreply.github.com>
2025-02-28 15:33:27 +0300

    Time: 3 ms (13.45%) | Memory: 17.6 MB (97.15%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -5,12 +5,10 @@ class Solution:
         """
         n=len(nums)
         for i in range(n-1):
-            swapped=False
-            for j in range(n-i-1):
+           for j in range(n-i-1):
                 if nums[j]>nums[j+1]:
                     nums[j],nums[j+1]=nums[j+1],nums[j]
                     swapped=True
-            if not swapped:
-                break
+            
 
 
